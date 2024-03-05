7b4e70c01c24c478dd40370afc3d44f91dbae0f6
Keisya Intan Nabila <128314097+Keisya31@users.noreply.github.com>
2024-03-05 20:13:46 +0700

    Delete Tugas Standar directory

--- Tugas Standar/AngkaSialException.java
+++
@@ -1,9 +0,0 @@
-/* File: AngkaSialException.java 
- * Deskripsi: Eksepsi buatan sendiri, menolak masukkan angka 13
-*/
-
-public class AngkaSialException extends Exception{
-    public AngkaSialException(){
-        super("jangan masukkan angka 13 karena siall !!!");
-    }
-}
